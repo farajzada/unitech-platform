c99dfe6e3ab6cbf53e65202df2821a53af1cb84a
farajzada <mfarajzadaa@gmail.com>
2025-06-28 02:49:54 +0400

    MsBffApplication attached

---
+++ ms-bff/src/main/java/az/unitech/msbff/MsBffApplication.java
@@ -0,0 +1,11 @@
+package az.unitech.msbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBffApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsBffApplication.class, args);
+    }
+}
