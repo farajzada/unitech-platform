8a339c3e20afb47c5a99e34c1c897ec0aa9525e1
farajzada <mfarajzadaa@gmail.com>
2025-06-28 02:49:38 +0400

    MsAccountApplication attached

---
+++ ms-account/src/main/java/az/unitech/msaccount/MsAccountApplication.java
@@ -0,0 +1,11 @@
+package az.unitech.msaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsAccountApplication.class, args);
+    }
+}
