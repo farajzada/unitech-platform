91a9f79e5d7c18ce3f06c3e489dc31daeb25705e
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-07-15 23:22:46 +0400

    CurrencyApiClient and RateSyncService

--- ms-currency/src/main/java/az/unitech/mscurrency/client/CurrencyApiClient.java
+++ ms-currency/src/main/java/az/unitech/mscurrency/client/CurrencyApiClient.java
@@ -1,4 +1,14 @@
 package az.unitech.mscurrency.client;
 
-public class CurrencyApiClient {
+import az.unitech.mscurrency.dto.response.ApiResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "currency-api",url="https://api.exchangeratesapi.io/v1")
+public interface CurrencyApiClient {
+
+    @GetMapping("/latest")
+    ApiResponseDto getLatestRates(@RequestParam("access_key") String accessKey,@RequestParam("format") int format);
+
 }
--- ms-currency/src/main/java/az/unitech/mscurrency/service/sync/RateSyncService.java
+++ ms-currency/src/main/java/az/unitech/mscurrency/service/sync/RateSyncService.java
@@ -1,4 +1,53 @@
 package az.unitech.mscurrency.service.sync;
 
+import az.unitech.mscurrency.client.CurrencyApiClient;
+import az.unitech.mscurrency.domain.entity.ExchangeRate;
+import az.unitech.mscurrency.domain.repository.ExchangeRateRepo;
+import az.unitech.mscurrency.dto.response.ApiResponseDto;
+import az.unitech.mscurrency.exception.CurrencyFetchException;
+import az.unitech.mscurrency.exception.RateNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
 public class RateSyncService {
+    private final CurrencyApiClient currencyApiClient;
+    private final ExchangeRateRepo exchangeRateRepo;
+
+    @Scheduled(fixedRate = 3600000)
+    public void sync() {
+        ApiResponseDto responseDto = currencyApiClient.getLatestRates("4551f9fa9b38ad61327f6d4d5a6d380c", 1);
+
+        boolean success = responseDto.isSuccess();
+        Map<String, BigDecimal> rates = responseDto.getRates();
+
+        if (!success || rates == null || rates.isEmpty()) {
+            throw new CurrencyFetchException("Failed to fetch currency data from external API");
+        }
+        String base = responseDto.getBase();
+        Long timestamp = responseDto.getTimestamp();
+        LocalDate date = LocalDate.now();
+
+        List<ExchangeRate> exchangeRateList = new ArrayList<>();
+
+        for (Map.Entry<String, BigDecimal> entry : rates.entrySet()) {
+            ExchangeRate exchangeRate = ExchangeRate.builder()
+                    .rate(entry.getValue())
+                    .timestamp(timestamp)
+                    .baseCurrency(base)
+                    .targetCurrency(entry.getKey())
+                    .date(date)
+                    .build();
+            exchangeRateList.add(exchangeRate);
+        }
+        exchangeRateRepo.saveAll(exchangeRateList);
+    }
 }
