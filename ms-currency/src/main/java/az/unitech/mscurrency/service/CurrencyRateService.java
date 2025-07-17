3bee91bf88bcd9a5088d6c5d4875be4a32daa7ed
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-07-17 23:09:59 +0400

    CurrencyRateService

--- ms-currency/src/main/java/az/unitech/mscurrency/service/CurrencyRateService.java
+++ ms-currency/src/main/java/az/unitech/mscurrency/service/CurrencyRateService.java
@@ -1,4 +1,16 @@
 package az.unitech.mscurrency.service;
 
+import az.unitech.mscurrency.dto.request.CreateRateRequest;
+import az.unitech.mscurrency.dto.response.ExchangeRateDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface CurrencyRateService {
+    List<ExchangeRateDto> getAllRates();
+    ExchangeRateDto createRate(CreateRateRequest createRateRequest);
+    ExchangeRateDto getRate(String from, String to);
+    ExchangeRateDto updateRate(String from, String to, BigDecimal rate);
+    void deleteRate(String from, String to);
+    void deleteRateById(Long id);
 }
--- ms-currency/src/main/java/az/unitech/mscurrency/service/impl/CurrencyRateServiceImpl.java
+++ ms-currency/src/main/java/az/unitech/mscurrency/service/impl/CurrencyRateServiceImpl.java
@@ -1,4 +1,40 @@
 package az.unitech.mscurrency.service.impl;
 
-public class CurrencyRateServiceImpl {
+import az.unitech.mscurrency.dto.request.CreateRateRequest;
+import az.unitech.mscurrency.dto.response.ExchangeRateDto;
+import az.unitech.mscurrency.service.CurrencyRateService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CurrencyRateServiceImpl implements CurrencyRateService {
+    @Override
+    public List<ExchangeRateDto> getAllRates() {
+        return List.of();
+    }
+
+    @Override
+    public ExchangeRateDto createRate(CreateRateRequest createRateRequest) {
+        return null;
+    }
+
+    @Override
+    public ExchangeRateDto getRate(String from, String to) {
+        return null;
+    }
+
+    @Override
+    public ExchangeRateDto updateRate(String from, String to, BigDecimal rate) {
+        return null;
+    }
+
+    @Override
+    public void deleteRate(String from, String to) {
+
+    }
+
+    @Override
+    public void deleteRateById(Long id) {
+
+    }
 }
