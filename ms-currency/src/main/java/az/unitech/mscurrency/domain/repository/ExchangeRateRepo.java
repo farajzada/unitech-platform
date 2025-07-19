fd379e7e859aa5b5ed4b1aea4a5095f0eb07d0e7
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-07-19 17:04:36 +0400

    service and repo

--- ms-currency/src/main/java/az/unitech/mscurrency/domain/repository/ExchangeRateRepo.java
+++ ms-currency/src/main/java/az/unitech/mscurrency/domain/repository/ExchangeRateRepo.java
@@ -3,5 +3,8 @@ package az.unitech.mscurrency.domain.repository;
 import az.unitech.mscurrency.domain.entity.ExchangeRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExchangeRateRepo extends JpaRepository<ExchangeRate,Long> {
+    Optional<ExchangeRate> findByBaseCurrencyAndTargetCurrency(String baseCurrency, String targetCurrency);
 }
--- ms-currency/src/main/java/az/unitech/mscurrency/service/impl/CurrencyRateServiceImpl.java
+++ ms-currency/src/main/java/az/unitech/mscurrency/service/impl/CurrencyRateServiceImpl.java
@@ -1,40 +1,71 @@
 package az.unitech.mscurrency.service.impl;
 
+import az.unitech.mscurrency.domain.entity.ExchangeRate;
+import az.unitech.mscurrency.domain.repository.ExchangeRateRepo;
 import az.unitech.mscurrency.dto.request.CreateRateRequest;
 import az.unitech.mscurrency.dto.response.ExchangeRateDto;
+import az.unitech.mscurrency.exception.RateNotFoundException;
+import az.unitech.mscurrency.mapper.ExchangeRateMapper;
 import az.unitech.mscurrency.service.CurrencyRateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class CurrencyRateServiceImpl implements CurrencyRateService {
+
+    private final ExchangeRateRepo exchangeRateRepo;
+    private final ExchangeRateMapper exchangeRateMapper;
+
     @Override
+    @Cacheable("rates")
     public List<ExchangeRateDto> getAllRates() {
-        return List.of();
+        List<ExchangeRate> list= exchangeRateRepo.findAll();
+        return list.stream()
+                .map(exchangeRateMapper::toDto)
+                .collect(Collectors.toList());
     }
 
+    @Cacheable("rates")
     @Override
     public ExchangeRateDto createRate(CreateRateRequest createRateRequest) {
-        return null;
+        ExchangeRate exchangeRate=exchangeRateMapper.toEntity(createRateRequest);
+        ExchangeRate savedRate=exchangeRateRepo.save(exchangeRate);
+        return exchangeRateMapper.toDto(savedRate);
     }
 
     @Override
+    @Cacheable("rates")
     public ExchangeRateDto getRate(String from, String to) {
-        return null;
+        ExchangeRate exchangeRate=rateFind(from,to);
+        return exchangeRateMapper.toDto(exchangeRate);
     }
 
     @Override
     public ExchangeRateDto updateRate(String from, String to, BigDecimal rate) {
-        return null;
+        ExchangeRate exchangeRate=rateFind(from, to);
+        exchangeRate.setRate(rate);
+        ExchangeRate savedRate=exchangeRateRepo.save(exchangeRate);
+        return exchangeRateMapper.toDto(savedRate);
     }
 
     @Override
     public void deleteRate(String from, String to) {
-
+        ExchangeRate exchangeRate=rateFind(from, to);
+        exchangeRateRepo.delete(exchangeRate);
     }
 
     @Override
     public void deleteRateById(Long id) {
-
+        exchangeRateRepo.deleteById(id);
     }
+
+    public ExchangeRate rateFind(String from,String to){
+        return exchangeRateRepo.findByBaseCurrencyAndTargetCurrency(from,to)
+                .orElseThrow(()-> new RateNotFoundException("Exchange rate not found for "+from+" to "+to));    }
 }
